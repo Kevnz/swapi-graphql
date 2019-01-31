277ffdfa71e2da8494a1b012de51da52ec877c9c
Kevin Isom <kevin.isom@gmail.com>
2019-02-01 11:58:15 +1300

    add sentry

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,12 @@
+const PORT = process.env.PORT
 require('xtconf')()
 const Path = require('path')
 const { ApolloServer } = require('apollo-server-hapi')
 const Hapi = require('hapi')
+const Sentry = require('@sentry/node')
+
+Sentry.init({ dsn: process.env.SENTRY })
+
 const Manifest = require('./manifest')
 const Types = require('./graphql/types')
 const Resolvers = require('./graphql/resolvers')
@@ -16,7 +21,7 @@ const start = async () => {
       playground: true,
     })
     app = Hapi.server({
-      port: process.env.PORT,
+      port: PORT || process.env.PORT,
       routes: {
         files: {
           relativeTo: Path.join(__dirname, 'public'),
