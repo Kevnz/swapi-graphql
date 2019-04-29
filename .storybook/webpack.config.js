8bfb7410e05794f8da1f49afdb95b7cc7b181949
Kevin Isom <kevin@isom.nz>
2019-04-30 08:05:41 +1200

    storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,27 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        loader: 'style-loader!css-loader!resolve-url-loader!sass-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.mjs', '.js', '.jsx'],
+    modules: ['node_modules', 'src'],
+    alias: {},
+  },
   plugins: [
     // your custom plugins
   ],
-  module: {
-    rules: [
-      // add your custom rules.
-    ],
-  },
 };
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo'
+import Film from '../src/ui/components/film'
+import Character from '../src/ui/components/character'
+import Vehicle from '../src/ui/components/vehicle'
+import Starship from '../src/ui/components/starship'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
+const film = require('./data/films.json')[0]
+const character = require('./data/characters.json')[0]
+const vehicle = require('./data/vehicles.json')[0]
+const starship = require('./data/starships.json')[0]
 
+storiesOf('Film', module).add('Episode 4', () => <Film {...film} />)
+
+storiesOf('Character', module).add('Luke', () => <Character {...character} />)
+
+storiesOf('Vehicle', module).add('Vehicle', () => <Vehicle {...vehicle} />)
+
+storiesOf('Starship', module).add('Starship', () => <Starship {...starship} />)
+
+/*
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
+  ))
+
+*/
