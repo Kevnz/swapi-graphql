abc5a494439a96996217c622f8836902e0c35767
Kevin Isom <kevin@isom.nz>
2019-04-30 10:53:36 +1200

    correct dom work

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,7 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './core/app'
 
-ReactDOM.render(
-  <App />,
-  document.body.appendChild(document.createElement('div'))
-)
+ReactDOM.render(<App />, document.getElementById('root'))
