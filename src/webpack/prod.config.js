f8841cac62f450019cd93f6a792af9effeb165e4
Kevin Isom <kevin@isom.nz>
2019-04-30 10:20:23 +1200

    Copy assets

--- src/webpack/prod.config.js
+++ src/webpack/prod.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const baseConfig = require('./config')
 const prodConfig = {
   plugins: [
@@ -8,6 +10,12 @@ const prodConfig = {
       template: './src/ui/index.html',
       historyApiFallback: true,
     }),
+    new CopyPlugin([
+      {
+        from: path.join(process.cwd(), '/src/public'),
+        to: path.join(process.cwd(), '/dist'),
+      },
+    ]),
   ],
   output: {
     path: path.join(process.cwd(), '/dist'),
