5ed403a0c8280eb7020a0e17bea27a2bb1d8e7b7
Kevin Isom <kevin@isom.nz>
2019-04-30 16:46:31 +1200

    serve dev content

--- src/webpack/dev.config.js
+++ src/webpack/dev.config.js
@@ -1,22 +1,33 @@
+const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseConfig = require('./config')
 const { env } = process
 const devConfig = {
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: './src/ui/index.html',
+      historyApiFallback: true,
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
     }),
-    new HtmlWebpackPlugin(),
   ],
   devtool: 'cheap-module-eval-source-map',
   devServer: {
+    contentBase: path.join(process.cwd(), '/src', '/public'),
     historyApiFallback: true,
     hot: true,
     proxy: {
       '/graphql': 'http://localhost:4545/',
       '/api': 'http://localhost:4545/',
     },
+  },
+  output: {
+    path: path.join(process.cwd(), '/out'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
 }
 
