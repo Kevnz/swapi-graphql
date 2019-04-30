5917853ee2f564328c94aa6f43dc77c5649fa117
Kevin Isom <kevin@isom.nz>
2019-04-30 17:50:39 +1200

    Loading spinner

---
+++ src/ui/components/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="spaced">
+      <p>Loading</p>
+      <span className="loader loader-xl" />
+    </div>
+  )
+}
