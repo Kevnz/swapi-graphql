2920f27a4dd778d1e2c68d1d026240f5e68a7c60
Kevin Isom <kevin@isom.nz>
2019-04-30 16:44:12 +1200

    Move Nav out

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -1,12 +1,7 @@
 import React from 'react'
-import { Link } from '@reach/router'
 
 export default () => (
   <main>
-    <h1>Star Wars GraphQL</h1>
-    <nav>
-      <Link to="/films">Films</Link>
-      <Link to="/films-at-rest">Rest API Films</Link>
-    </nav>
+    <h2>Choose</h2>
   </main>
 )
