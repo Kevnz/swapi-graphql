ea80ce195a57f4e41fb7f45e8a6cb067ca3d0cac
Kevin Isom <kevin@isom.nz>
2019-04-30 17:50:55 +1200

    character

--- src/ui/core/routes.js
+++ src/ui/core/routes.js
@@ -4,6 +4,7 @@ import { Router } from '@reach/router'
 const Home = React.lazy(() => import('../features/home'))
 const Contact = React.lazy(() => import('../features/contact'))
 const Films = React.lazy(() => import('../features/films'))
+const Characters = React.lazy(() => import('../features/characters'))
 const RestFilms = React.lazy(() => import('../features/films-rest'))
 export default () => {
   return (
@@ -11,6 +12,7 @@ export default () => {
       <Home path="/" />
       <Films path="/films" />
       <RestFilms path="/films-at-rest" />
+      <Characters path="/characters" />
       <Contact path="/contact" />
     </Router>
   )
