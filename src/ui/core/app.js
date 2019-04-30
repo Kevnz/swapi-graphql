8b12a5fc5b7c3238a5f42e82be6aaef3b2916f11
Kevin Isom <kevin@isom.nz>
2019-04-30 16:43:59 +1200

    Nav links

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,11 +1,34 @@
 import React, { Fragment } from 'react'
+import { Link } from '@reach/router'
 import Routes from './routes'
 
 const App = () => <Routes />
+const NavLink = props => (
+  <Link
+    {...props}
+    getProps={({ isCurrent }) => {
+      // the object returned here is passed to the
+      // anchor element's props
+      return {
+        className: isCurrent ? 'button active' : 'button',
+      }
+    }}
+  />
+)
 export default () => {
   return (
     <Fragment>
-      <h1>SWAPI</h1>
+      <h1>
+        <Link to="/">SWAPI</Link>
+      </h1>
+      <nav>
+        <NavLink to="/films" className="button">
+          Films
+        </NavLink>
+        <NavLink to="/films-at-rest" className="button">
+          Rest API Films
+        </NavLink>
+      </nav>
       <React.Suspense fallback={<div>Loading</div>}>
         <App />
       </React.Suspense>
