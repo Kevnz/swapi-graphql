86e460a8931ff49239469a1dca8edb1f56add70d
Kevin Isom <kevin@isom.nz>
2019-05-01 16:39:28 +1200

    no logging

--- src/server/utils/__tests__/swapi-test.js
+++ src/server/utils/__tests__/swapi-test.js
@@ -2,10 +2,8 @@ const swapi = require('../swapi')
 
 describe('The SWAPI API', async () => {
   it('should get Luke Skywalker', async () => {
-    console.warn('swap', swapi)
     const person = await swapi.people(1)
-    console.warn('person', person)
-    expect(person.name).toBe('Luke Skywalker')
 
+    expect(person.name).toBe('Luke Skywalker')
   })
 })
