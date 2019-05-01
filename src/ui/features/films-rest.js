0b17fdfaf07ea5e66c5f9286ea24cfa6033bbe0c
Kevin Isom <kevin@isom.nz>
2019-05-01 16:35:48 +1200

    Header and use character loading component

--- src/ui/features/films-rest.js
+++ src/ui/features/films-rest.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { mapper, each } from '@kev_nz/async-tools'
 import useGet from '../tools/use-get'
 import Film from '../components/film'
+import Characters from '../components/rest/characters'
 import Loading from '../components/loading'
 const Films = () => {
   const [id, setId] = useState(1)
@@ -19,11 +21,15 @@ const Films = () => {
       return a.episode > b.episode ? 1 : -1
     })
     .map(f => {
-      return <Film key={`ep-${f.episode}`} {...f} />
+      return (
+        <Film key={`ep-${f.episode}`} {...f}>
+          <Characters filmId={f.id} />
+        </Film>
+      )
     })
   return (
     <main>
-      <h3>Films from the Star Wars API</h3>
+      <h3>Films from the Star Wars REST API</h3>
       {films}
     </main>
   )
