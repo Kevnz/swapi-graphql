18bd53b8b772db2d8ec8fd675566353646a6a141
Kevin Isom <kevin@isom.nz>
2019-05-01 16:40:17 +1200

    no logging

--- src/server/graphql/types.js
+++ src/server/graphql/types.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs')
 const path = require('path')
 const { gql } = require('apollo-server-hapi')
@@ -6,18 +5,16 @@ const { gql } = require('apollo-server-hapi')
 const schemaFiles = fs.readdirSync(path.join(__dirname, 'schemas'))
 
 const schemas = schemaFiles.reduce((previous, current) => {
-  console.log('current', current)
   if (current.indexOf('.graphql') === -1) return previous
   const schema = fs.readFileSync(
     path.join(__dirname, 'schemas', current),
     'utf8'
   )
-  return`${previous}
+  return `${previous}
   ${schema}
   `
 }, '')
 
-console.log('schemas', schemas)
 const typeDefs = gql`
   ${schemas}
 `
