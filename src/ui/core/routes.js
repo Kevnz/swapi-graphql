1edd6aa60ef974996d977103b27cafbea9adf72e
Kevin Isom <kevin@isom.nz>
2019-05-01 16:36:38 +1200

    Add new routes

--- src/ui/core/routes.js
+++ src/ui/core/routes.js
@@ -5,7 +5,14 @@ const Home = React.lazy(() => import('../features/home'))
 const Contact = React.lazy(() => import('../features/contact'))
 const Films = React.lazy(() => import('../features/films'))
 const Characters = React.lazy(() => import('../features/characters'))
+const Vehicles = React.lazy(() => import('../features/vehicles'))
+const Starships = React.lazy(() => import('../features/starships'))
+const Species = React.lazy(() => import('../features/species'))
+const RestCharacters = React.lazy(() => import('../features/characters-rest'))
 const RestFilms = React.lazy(() => import('../features/films-rest'))
+const RestVehicles = React.lazy(() => import('../features/vehicles-rest'))
+const RestStarships = React.lazy(() => import('../features/starships-rest'))
+const RestSpecies = React.lazy(() => import('../features/species-rest'))
 export default () => {
   return (
     <Router>
@@ -13,6 +20,13 @@ export default () => {
       <Films path="/films" />
       <RestFilms path="/films-at-rest" />
       <Characters path="/characters" />
+      <Vehicles path="/vehicles" />
+      <Starships path="/starships" />
+      <Species path="/species" />
+      <RestCharacters path="/characters-at-rest" />
+      <RestVehicles path="/vehicles-at-rest" />
+      <RestStarships path="/starships-at-rest" />
+      <RestSpecies path="/species-at-rest" />
       <Contact path="/contact" />
     </Router>
   )
