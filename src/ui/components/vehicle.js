fb5859724b34cbdae566dedb310d5e89459ad2f0
Kevin Isom <kevin@isom.nz>
2019-05-01 16:37:11 +1200

    Update Vehicle data

--- src/ui/components/vehicle.js
+++ src/ui/components/vehicle.js
@@ -3,10 +3,14 @@ import React from 'react'
 export default function Vehicle({ name, manufacturer, model }) {
   return (
     <div className="spaced">
-      <h3>
-        {manufacturer} {name}
-      </h3>
-      <span>{model}</span>
+      <div className="spaced">
+        <h4>
+          The {manufacturer} {name}
+        </h4>
+        <span>
+          <em>Model:</em> {model}
+        </span>
+      </div>
     </div>
   )
 }
