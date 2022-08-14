078c36d3d6c4849fe1d345321d99e74629323412
Kevin <kevin.isom@gmail.com>
2022-08-15 10:41:19 +1200

    stuff and things

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,11 @@ module.exports = {
   development: config.get('database'),
   test: config.get('database'),
   staging: config.get('database'),
-  production: config.get('database'),
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './src/migrations',
+    },
+  },
 }
